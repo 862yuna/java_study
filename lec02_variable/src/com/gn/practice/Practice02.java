d656d59e8ffa02c31cc5d7b3f8de1e3420765d6b
862yuna <862yuna@gmail.com>
2024-11-27 10:21:22 +0900

    [cya] 철수의 일기

---
+++ lec02_variable/src/com/gn/practice/Practice02.java
@@ -0,0 +1,24 @@
+package com.gn.practice;
+
+public class Practice02 {
+
+	public static void main(String[] args) {
+		boolean isStudent = true;
+		System.out.println(isStudent);
+		char grade = 'A';
+		System.out.println(grade);
+		byte candy = 3;
+		System.out.println(candy);
+		short walk = 2000;
+		System.out.println(walk);
+		int day = 3;
+		System.out.println(day);
+		long future = 100;
+		System.out.println(future);
+		float height = 177.7f;
+		System.out.println(height);
+		double score = 4.1;
+		System.out.println(score);
+		
+	}
+}
