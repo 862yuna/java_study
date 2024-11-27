86e216a18eb2854903941cb975dcc7b56bc3751a
862yuna <862yuna@gmail.com>
2024-11-27 11:10:51 +0900

    [cya] charAt() 제갈민의 이름

---
+++ lec02_variable/src/com/gn/practice/Practice03.java
@@ -0,0 +1,10 @@
+package com.gn.practice;
+
+public class Practice03 {
+	public static void main(String[] agrs) {
+		String name = "제갈민";
+		char firstName = name.charAt(2);
+		System.out.println(firstName + "이에요!!");
+	}
+
+}
