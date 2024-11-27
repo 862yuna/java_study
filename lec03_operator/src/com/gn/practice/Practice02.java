07cb9e6691e0842f5b187ddd44e7fc6b381ebda3
862yuna <862yuna@gmail.com>
2024-11-27 19:18:02 +0900

    [cya] lec03_operator
    	- Practice02

--- lec03_operator/src/com/gn/practice/Practice02.java
+++ lec03_operator/src/com/gn/practice/Practice02.java
@@ -12,8 +12,12 @@ public class Practice02 {
 		System.out.println(result); //186
 		result /= 3;
 		System.out.println(result); //62
-		result %= 10;
+		result %= 3;
 		System.out.println(result); //2
+		
+		// 홀수, 짝수
+		// 1, 3, 5, ... -> 2로 나눴을 때 나머지 1
+		// 2, 4, 6, ... -> 2로 나눴을 때 나머지 0
 		
 
 	}
