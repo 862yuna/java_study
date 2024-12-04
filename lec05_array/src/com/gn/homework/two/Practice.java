3399131f6407f9125f9d8b8d7a6f374d4c371c4a
862yuna <862yuna@gmail.com>
2024-12-04 23:57:53 +0900

    [cya] lec05_array
    	-homework 수정

--- lec05_array/src/com/gn/homework/two/Practice.java
+++ lec05_array/src/com/gn/homework/two/Practice.java
@@ -30,45 +30,73 @@ public class Practice {
 	
 	public void practice03() {
 		Scanner sc = new Scanner(System.in);
+		int num1 = 0;
+		int num2 = 0;
 		while(true) {
-			System.out.print("행 : ");
-			int num1 = sc.nextInt();
-			System.out.print("열 : ");
-			int num2 = sc.nextInt();
-			
-			int[][] arr = new int[num1][num2];
-			
-			if(0==num1||num1>10||0==num2||num2>10) {
-				System.out.println("반드시 1~10 사이의 정수를 입력해야 합니다.");
+			if(!(1<=num1&&num1<=10)) {
+				System.out.println("반드시 1~10 사이의 정수를 입력해야합니다.");
+				System.out.print("행 : ");
+				num1 = sc.nextInt();
 			} else {
-				for(int i =0;i<num1;i++) {
-					for(int j=0;j<num2;j++) {
-						int random = (int)(Math.random()*26)+65;
-						arr[i][j] = (char)random;
-						System.out.print(arr[i][j]+ " ");
-					}
-					System.out.print("\n");
-				}
+				break;
 			}
-			
+		}
+		while(true) {
+			if(!(1<=num2&&num2<=10)) {
+				System.out.println("반드시 1~10 사이의 정수를 입력해야합니다.");
+				System.out.print("열 : ");
+				num2 = sc.nextInt();
+			} else {
+				break;
+			}
+		}
+		char[][] arr = new char[num1][num2];
+		for(int i =0;i<num1;i++) {
+			for(int j=0;j<num2;j++) {
+				int random = (int)(Math.random()*26)+65;
+				arr[i][j] = (char)random;
+				System.out.print(arr[i][j]+ " ");
+				
+			}
+			System.out.print("\n");
+		}
+		sc.close();
+	}
+	
+	
+	public void practice04() {
+		String[][] strArr = new String[][] {{"이","까","왔","앞","힘"},
+			{"차","지","습","으","냅"},{"원","열","니","로","시"},
+			{"배","심","다","좀","다"},{"열","히","! ","더","!! "}};
+		for(int i = 0;i<strArr.length;i++) {
+			for(int j =0;j<strArr[i].length;j++) {
+//				strArr = new String[j][i];
+				System.out.print(strArr[j][i]+ " ");
+			}
+			System.out.print("\n");
+		}
+	}
+	
+	
+	public void practice05() {
+		String[] checkName = {"강건강","남나나","도대담","류라라","문미미","박보배",
+				"송성실","윤예의","진재주","차천축","피풍표","홍하하"};
+		String[][] names = new String[3][2];
+		for(int i =0;i<checkName.length;i++) {
+			if(i<6) {
+				
+			}
+		}
+		for(int i=0;i<names.length;i++) {
+			for(int j=0;j<names[i].length;j++) {
+				
+			}
+		
+		
+		
+		
 		}
 		
-//		int random = (int)(Math.random()*26)+65;
-//		char alphabet = (char)random;
-//		arr[i][j] = alphabet;
-//		System.out.print(arr[i][j]+" ");
-		
-		
-		
-		
-		
-		
-		
-	}
-	public void practice04() {
-		
-	}
-	public void practice05() {
 		
 	}
 	public void practice06() {
