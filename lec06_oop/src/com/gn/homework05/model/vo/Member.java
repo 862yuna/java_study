665a47eaaf3469a97a6a3ad43b61087e4a572de6
862yuna <862yuna@gmail.com>
2024-12-05 17:46:01 +0900

    [cya] lec06_opp
    	-객체 과제

---
+++ lec06_oop/src/com/gn/homework05/model/vo/Member.java
@@ -0,0 +1,25 @@
+package com.gn.homework05.model.vo;
+
+public class Member {
+	private String memberId;
+	private String memberPwd;
+	private String memberName;
+	private int age;
+	private char gender;
+	private String phone;
+	private String email;
+	
+	public Member() {
+		
+	}
+	
+	public void changeName(String name) {
+		
+	}
+	
+	public void printName() {
+		
+	}
+	
+
+}
